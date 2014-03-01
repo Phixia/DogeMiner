e7de424232fd5c245268d028969412a93f4201b7
phixia <phixia@SlaveI.(none)>
2014-03-01 09:14:29 -0600

    some additonal automation

---
+++ servconf.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+# This script preforms some really basic server conf
+#Anders Nelson 3-1-2014
+
+/bin/mv /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
+
+/bin/mv newssh_config /etc/ssh/sshd_config
+
+#more to come?
