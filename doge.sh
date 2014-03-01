97b7e76565f63a20123d4220e2d64d500275b992
phixia <phixia@SlaveI.(none)>
2014-03-01 09:05:12 -0600

    untested changes to add user

--- doge.sh
+++ doge.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
+/usr/sbin/useradd -m -s /bin/bash dogeminer
+
 /usr/bin/apt-get update
 /usr/bin/apt-get install -y build-essential autotools-dev libcurl4-gnutls-dev autoconf automake screen
 
+/bin/mkdir -p /home/dogeminer/.ssh/ && cp dogeminer.authorized_keys /home/dogeminer/.ssh/authorized_keys
+
+/bin/chown -R dogeminer:dogeminer /home/dogeminer && /bin/chmod 600 /home/dogeminer/.ssh/authorized_keys
+
+/bin/su - dogeminer
+
 cd ~/
+
 /usr/bin/git clone https://github.com/pooler/cpuminer.git
+
 cd cpuminer/
+
 bash autogen.sh
+
 CFLAGS="-O3 -Wall -msse2" ./configure
+
 /usr/bin/make
+
 screen -S muchcoin
 
